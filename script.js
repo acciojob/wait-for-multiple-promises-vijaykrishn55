92a989cd290756438112e55ad758753f3b3518d7
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-08-17 17:26:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,68 +1,53 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded", () => {
-  const outputTable = document.getElementById("output");
+const output = document.getElementById("output");
 
-  // Helper function to generate a random time between min and max seconds
-  function getRandomTime(min, max) {
-    return Math.random() * (max - min) * 1000 + min * 1000;
-  }
+const createPromises = [
+  getRandom('Promise 1'),
+  getRandom('Promise 2'),
+  getRandom('Promise 3')
+];
 
-  // Helper function to create a Promise that resolves after a random time
-  function createRandomPromise(promiseName) {
-    return new Promise((resolve) => {
-      const startTime = Date.now();
-      const randomTime = getRandomTime(1, 3);
-      setTimeout(() => {
-        const endTime = Date.now();
-        const timeTaken = (endTime - startTime) / 1000;
-        resolve({ promiseName, timeTaken });
-      }, randomTime);
-    });
-  }
+function randomTime(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
-  // Create an array of Promises
-  const promises = [
-    createRandomPromise("Promise 1"),
-    createRandomPromise("Promise 2"),
-    createRandomPromise("Promise 3"),
-  ];
+function getRandom(solve) {
+  return new Promise((resolve) => {
+    const time = randomTime(1, 3) * 1000; // Convert to milliseconds
+    setTimeout(() => {
+      resolve({ promiseName: solve, timeTaken: time / 1000 }); // Resolve with an object containing promiseName and timeTaken
+    }, time);
+  });
+}
 
-  // Display "Loading..." initially
-   
+Promise.all(createPromises)
+  .then((results) => {
+    const totalTime = results.reduce((total, result) => total + result.timeTaken, 0);
 
-  // Wait for all promises to resolve using Promise.all
-  Promise.all(promises)
-    .then((results) => {
-      // Calculate the total time taken
-      const totalTime = results.reduce((total, result) => total + result.timeTaken, 0);
-
-      // Update the table with the results
-		outputTable.innerHTML = "";
-      outputTable.innerHTML = `
-        <tr>
-          <td>${results[0].promiseName}</td>
-          <td>${results[0].timeTaken.toFixed(3)}</td>
-        </tr>
-        <tr>
-          <td>${results[1].promiseName}</td>
-          <td>${results[1].timeTaken.toFixed(3)}</td>
-        </tr>
-        <tr>
-          <td>${results[2].promiseName}</td>
-          <td>${results[2].timeTaken.toFixed(3)}</td>
-        </tr>
-        <tr>
-          <td>Total</td>
-          <td>${totalTime.toFixed(3)}</td>
-        </tr>
-      `;
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      outputTable.innerHTML = `
-        <tr>
-          <td colspan="2">Error occurred</td>
-        </tr>
-      `; 
-    });
-});
+    output.innerHTML = "";
+    output.innerHTML = `
+      <tr>
+        <td>${results[0].promiseName}</td>
+        <td>${results[0].timeTaken.toFixed(3)}</td>
+      </tr>
+      <tr>
+        <td>${results[1].promiseName}</td>
+        <td>${results[1].timeTaken.toFixed(3)}</td>
+      </tr>
+      <tr>
+        <td>${results[2].promiseName}</td>
+        <td>${results[2].timeTaken.toFixed(3)}</td>
+      </tr>
+      <tr>
+        <td>Total</td>
+        <td>${totalTime.toFixed(3)}</td>
+      </tr>
+    `;
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+    output.innerHTML = `
+      <tr>
+        <td colspan="2">Error occurred</td>
+      </tr>
+    `;
+  });
